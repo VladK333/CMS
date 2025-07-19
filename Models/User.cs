ef271a61294b171d9c54dcfb7a4f5ba9f2c6afc3
VladK333 <vladanaaleksic3@gmail.com>
2025-07-19 13:41:06 +0200

    Models added

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content_Management_System.Models
+{
+    public enum UserRole { Visitor, Admin};
+    [Serializable]
+    public class User
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public UserRole UserRole { get; set; }
+
+        public User() { }
+
+        public User(string username, string password, UserRole userRole)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.UserRole = userRole;
+        }
+    }
+}
